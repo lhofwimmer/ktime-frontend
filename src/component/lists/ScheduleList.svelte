a528fbd1ae6d63b3523ad676c58c52c26c44b089
Lukas Hofwimmer <lukas.hofwimmer@gmail.com>
2020-07-26 22:34:00 +0200

    ADD pagination to schedule list

--- src/component/lists/ScheduleList.svelte
+++ src/component/lists/ScheduleList.svelte
@@ -1,10 +1,13 @@
 <script>
   // import { getAllSchedules } from "../../service/userservice.js";
   // import { onMount } from "svelte";
+  import { getAllSchedules } from "../../service/userservice";
 
   const dateFormatter = new Intl.DateTimeFormat("de-AT");
 
   export let schedules = [];
+
+  let offset = 0;
 
   // onMount(async () => {
   //   schedules = await getAllSchedules();
@@ -74,3 +77,26 @@
     </div>
   </div>
 </div>
+
+<nav
+  class="px-4 py-3 flex items-center justify-between border-t border-gray-200
+  sm:px-6">
+  <div class="flex-1 flex justify-between sm:justify-end">
+    <span
+      class="relative inline-flex items-center px-4 py-2 border border-gray-300
+      text-sm leading-5 font-medium rounded-md text-gray-700 bg-white
+      hover:text-gray-500 focus:outline-none focus:shadow-outline-blue
+      focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition
+      ease-in-out duration-150">
+      Previous
+    </span>
+    <span
+      class="ml-3 relative inline-flex items-center px-4 py-2 border
+      border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700
+      bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue
+      focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition
+      ease-in-out duration-150">
+      Next
+    </span>
+  </div>
+</nav>
