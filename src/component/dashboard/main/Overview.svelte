90714a75756997aba5fbb289cc5cde5c890dc374
Lukas Hofwimmer <lukas.hofwimmer@gmail.com>
2020-07-26 22:47:36 +0200

    ADD await spinners to overview

--- src/component/dashboard/main/Overview.svelte
+++ src/component/dashboard/main/Overview.svelte
@@ -5,14 +5,15 @@
   import ScheduleList from "../../lists/ScheduleList.svelte";
   import { onMount } from "svelte";
   import { getEmployees, getAllSchedules } from "../../../service/userservice";
+  import { Shadow } from "svelte-loading-spinners";
 
-  let employees = [];
-  let schedules = [];
+  let employees = getEmployees();
+  let schedules = getAllSchedules();
 
-  onMount(async () => {
-    employees = await getEmployees();
-    schedules = await getAllSchedules();
-  });
+  // onMount(async () => {
+  //   employees = await getEmployees();
+  //   schedules = await getAllSchedules();
+  // });
 
   // function handleAddEmployee(event) {
   //   employees = [...employees, event.detail];
@@ -23,23 +24,36 @@
   // }
 </script>
 
-<div class="overflow-y-scroll flex-1">
+<!-- <div class="overflow-y-scroll flex-1">
   <div
     class="flex-none flex flex-col items-center text-center bg-teal-900
     text-gray-400 border-r" />
-  <!-- <div class="row-auto">
+  
+</div> -->
+
+<!-- <div class="row-auto">
     <CreateEmployeeForm on:addemployee={handleAddEmployee} />
   </div> -->
-
-  <div class="row-auto">
-    <EmployeeList {employees} />
+{#await employees}
+  <div class="flex justify-center py-4">
+    <Shadow size="40" color="#5850ec" unit="px" />
   </div>
+{:then result}
+  <EmployeeList employees={result} />
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
 
-  <!-- <div class="row-auto">
+<!-- <div class="row-auto">
     <CreateScheduleForm {employees} on:addschedule={handleAddEmployee} />
   </div> -->
 
-  <div class="row-auto">
-    <ScheduleList {schedules} />
+{#await schedules}
+  <div class="flex justify-center py-4">
+    <Shadow size="40" color="#5850ec" unit="px" />
   </div>
-</div>
+{:then result}
+  <ScheduleList schedules={result} />
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
